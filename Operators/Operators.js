5efde489f3fe9da684d6a1b297be50c552412ced
Roshrosh756 <roshnisah543@gmail.com>
2025-06-21 10:13:17 +0545

    Updated

---
+++ Operators/Operators.js
@@ -0,0 +1,27 @@
+//Arithmetic Operators
+const sum = 5 + 3;      // Addition
+const diff = 10 - 2;   // Subtraction
+const p = 4 * 2;       // Multiplication
+const q = 8 / 2;       // Division
+console.log(sum, diff, p, q);       //output : 8 8 8 4
+
+//Assignment Operators
+let n = 10;         // = assigns a value to a variable
+n += 5;             // += adds and assign the result to variable
+n *= 2;             // *= multiply and assigns the result to the variable
+console.log(n);     // output : 30
+
+//Comparison Operators
+let a = 10;
+let b = '10';
+
+console.log(a == b);   // true  only compares value
+console.log(a === b);  // false  compares value AND type
+console.log(a != b);   // false
+console.log(a !== b);  // true
+console.log(a > 5);    // true
+console.log(a <= 9);   // false
+
+//String Operators
+console.log("Hello" + " World"); // Output : Hello World
+console.log("Age: " + 25);       //  Output : Age: 25
---
+++ Variables/notes.md
@@ -0,0 +1,27 @@
+Javascript variables can be declared in 4 ways :
+### Automatically
+### Using var
+### Using let
+### Using const
+
+When to use var, let, or const?
+1.Always declare varaiable
+2 Always use const if the value should not be changed
+3.Only use let if you can't use const
+
+Examples:
+# let
+ let age = 45;
+ console.log(age);      // Output : 45
+
+ # const
+ const pi = 3.14;
+ console.log(pi);        // Output: 3.14
+
+ # var
+  var name = Roshni;
+  console.var(name);      // Output: Roshni
+
+
+
+
