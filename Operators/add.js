558f0fb2a40700dd6d392df654403243d5f65d1a
Roshrosh756 <roshnisah543@gmail.com>
2025-06-23 00:32:22 +0545

    FirstGame

---
+++ Operators/add.js
@@ -0,0 +1,4 @@
+let n = 10;         // = assigns a value to a variable
+n += 5;             // += adds and assign the result to variable
+n *= 2;             // *= multiply and assigns the result to the variable
+console.log(n); 
