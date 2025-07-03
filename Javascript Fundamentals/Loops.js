ed9ef6546a96d0b167d3784715c813cd44c02925
Roshrosh756 <roshnisah543@gmail.com>
2025-07-04 01:16:41 +0545

    Practice

--- Javascript Fundamentals/Loops.js
+++ Javascript Fundamentals/Loops.js
@@ -7,3 +7,13 @@ for (let i = 1; i<=12; i++ ){
 console.log ("Loop has ended");
 
 
+//practice
+
+//print 1 to 10 sum
+
+let sum = 0;
+for (let i = 1 ; i<=10; i++) {
+    sum = sum + i;
+    
+}
+console.log ("sum is", sum);
